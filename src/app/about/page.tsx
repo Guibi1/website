66d05370fd88698ef629225c5986566cfda78b07
Laurent Stéphenne <laurent@guibi.dev>
2025-08-04 20:13:21 -0400

    About start

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+    return (
+        <main className="container px-4 py-8">
+            <header className="mb-12">
+                <h1 className="mb-2 font-bold text-4xl text-primary">About me</h1>
+                <p className="text-muted-foreground text-xl">My experience</p>
+            </header>
+        </main>
+    );
+}
