67eb4b16a7fc8699e48a4626ab2ba359c1cc2c83
Laurent Stéphenne <laurent@guibi.dev>
2025-08-08 12:40:58 -0400

    Nice masonery layout for the project page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,5 +1,10 @@
+import { GithubIcon, GitPullRequestArrowIcon } from "lucide-react";
 import type { Metadata } from "next";
-import ProjectList, { type Project } from "./ProjectList";
+import Link from "next/link";
+import PullRequestList from "@/components/PullRequestList";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
 
 export const metadata: Metadata = {
     title: "Projects • Guibi.dev",
@@ -13,53 +18,103 @@ export default function Page() {
                 <p className="text-muted-foreground text-xl">Here's what I'm working on</p>
             </header>
 
-            <ProjectList projects={projects} />
+            <div className="columns-xs gap-4">
+                {projects.map((project) => (
+                    <Card className="mb-4 break-inside-avoid" key={project.repo}>
+                        <CardHeader>
+                            <CardTitle>{project.title}</CardTitle>
+                        </CardHeader>
+
+                        <CardContent className="grow space-y-4">
+                            <div>{project.description}</div>
+
+                            {project.showPullRequests && (
+                                <div className="space-y-2 pb-4">
+                                    <Separator />
+
+                                    <div className="inline-flex size-lg items-center gap-2">
+                                        <GitPullRequestArrowIcon size={16} />
+                                        My Contributions
+                                    </div>
+
+                                    <PullRequestList repo={project.repo} />
+                                </div>
+                            )}
+
+                            <div className="flex flex-wrap gap-2">
+                                {project.tags.map((tag) => (
+                                    <span
+                                        key={tag}
+                                        className="rounded-full bg-secondary px-2 py-1 font-medium text-secondary-foreground text-xs"
+                                    >
+                                        {tag}
+                                    </span>
+                                ))}
+                            </div>
+                        </CardContent>
+
+                        <CardFooter>
+                            <Button variant="outline" className="grow md:grow-0" asChild>
+                                <Link
+                                    href={`https://github.com/${project.repo}`}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                >
+                                    <GithubIcon />
+                                    Code
+                                </Link>
+                            </Button>
+                        </CardFooter>
+                    </Card>
+                ))}
+            </div>
         </main>
     );
 }
 
-const projects: Project[] = [
+const projects: {
+    title: string;
+    description: string;
+    tags: string[];
+    repo: string;
+    showPullRequests?: boolean;
+}[] = [
     {
         title: "Maki",
         description: "A Bun-based React-19 metaframework for building typesafe and fast fullstack websites.",
-        details: "A Bun-based React-19 metaframework for building typesafe and fast fullstack websites.",
         tags: ["React", "Server components", "TypeScript"],
         repo: "Guibi1/maki",
     },
     {
-        title: "Pumpkin",
-        description: "A Minecraft server built entirely in Rust, offering a fast and customizable experience.",
-        details: "A Minecraft server built entirely in Rust, offering a fast, efficient, and customizable experience.",
-        tags: ["Rust", "Game Server"],
-        repo: "Pumpkin-MC/Pumpkin",
-        showPullRequests: true,
-    },
-    {
         title: "FireWall",
-        description: "A 2D battle royale game where player written bots compete against each other.",
-        details:
+        description:
             "A 2D battle royale game where player written bots compete against each other in a shrinking arena. Used in the 2025 JDIS Games competition.",
         tags: ["Rust", "Game Server", "React"],
         repo: "JDIS/jdis-games-2025",
     },
     {
+        title: "Pumpkin",
+        description:
+            "A Minecraft server built entirely in Rust, offering a fast, efficient, and customizable experience.",
+        tags: ["Rust", "OSS", "Game Server"],
+        repo: "Pumpkin-MC/Pumpkin",
+        showPullRequests: true,
+    },
+    {
         title: "SvelteKit TypeSafe API",
         description: "A Vite plugin that adds type safety to SvelteKit's server endpoints.",
-        details: "A Vite plugin that adds type safety to SvelteKit's server endpoints.",
         tags: ["SvelteKit", "TypeScript"],
         repo: "Guibi1/SvelteKit-TypeSafe-API",
     },
     {
         title: "Univox",
         description: "A student-only website to share schedules between friends and sell used books.",
-        details: "A student-only website to share schedules between friends and sell used books.",
         tags: ["SvelteKit", "PostgreSQL", "S3"],
         repo: "Guibi1/Univox",
     },
     {
         title: "Guibi.dev",
         description: "A handmade website to show-off what I've built.",
-        details: "A handmade website to show-off what I've built.",
         tags: ["Next.js", "TypeScript"],
         repo: "Guibi1/website",
     },
--- src/components/PullRequestList.tsx
+++ src/components/PullRequestList.tsx
@@ -1,6 +1,5 @@
-import { ExternalLinkIcon, GitPullRequestArrowIcon } from "lucide-react";
+import { ExternalLinkIcon } from "lucide-react";
 import { Suspense, useMemo } from "react";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { fetchPullRequests } from "@/lib/github";
 import TimeAgo from "./TimeAgo";
 import { Button } from "./ui/button";
@@ -10,54 +9,45 @@ export default function PullRequestList({ repo }: { repo: string }) {
     const prs = useMemo(() => fetchPullRequests(repo, "Guibi1"), [repo]);
 
     return (
-        <Card className="mx-auto max-w-5xl">
-            <CardHeader>
-                <CardTitle className="inline-flex items-center gap-4">
-                    <GitPullRequestArrowIcon />
-                    My Contributions
-                </CardTitle>
-            </CardHeader>
+        <div className="max-h-80 overflow-y-scroll">
+            <Suspense fallback={<PullRequestListSkeleton />}>
+                {prs.then((prs) =>
+                    prs.length === 0 ? (
+                        <div className="py-4 text-center text-muted-foreground">No pull requests found.</div>
+                    ) : (
+                        <ul className="flex flex-col divide-y divide-muted">
+                            {prs.map((pr) => (
+                                <li key={pr.id}>
+                                    <a
+                                        href={pr.url}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className="flex items-center justify-between rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring"
+                                    >
+                                        <div className="flex flex-col px-2">
+                                            <div>{pr.title}</div>
 
-            <CardContent className="max-h-90 overflow-y-scroll px-4">
-                <Suspense fallback={<PullRequestListSkeleton />}>
-                    {prs.then((prs) =>
-                        prs.length === 0 ? (
-                            <div className="py-4 text-center text-muted-foreground">No pull requests found.</div>
-                        ) : (
-                            <ul className="flex flex-col divide-y divide-muted">
-                                {prs.map((pr) => (
-                                    <li key={pr.id}>
-                                        <a
-                                            href={pr.url}
-                                            target="_blank"
-                                            rel="noopener noreferrer"
-                                            className="flex items-center justify-between rounded-md p-2 transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring"
-                                        >
-                                            <div className="flex flex-col px-2">
-                                                <div>{pr.title}</div>
-
-                                                <div className="flex items-center gap-2 text-muted-foreground text-sm">
-                                                    <span>#{pr.number}</span>
-                                                    <span>•</span>
-                                                    <span>
-                                                        {pr.mergedAt ? "merged" : pr.closedAt ? "closed" : "opened"}
-                                                    </span>
-                                                    <TimeAgo date={pr.mergedAt ?? pr.closedAt ?? pr.openedAt} />
-                                                </div>
+                                            <div className="flex items-center gap-2 text-muted-foreground text-sm">
+                                                <span>#{pr.number}</span>
+                                                <span>•</span>
+                                                <span>
+                                                    {pr.mergedAt ? "merged" : pr.closedAt ? "closed" : "opened"}
+                                                </span>
+                                                <TimeAgo date={pr.mergedAt ?? pr.closedAt ?? pr.openedAt} />
                                             </div>
+                                        </div>
 
-                                            <Button variant="ghost" size="icon" disabled>
-                                                <ExternalLinkIcon />
-                                            </Button>
-                                        </a>
-                                    </li>
-                                ))}
-                            </ul>
-                        ),
-                    )}
-                </Suspense>
-            </CardContent>
-        </Card>
+                                        <Button variant="ghost" size="icon" disabled>
+                                            <ExternalLinkIcon />
+                                        </Button>
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
+                    ),
+                )}
+            </Suspense>
+        </div>
     );
 }
 
